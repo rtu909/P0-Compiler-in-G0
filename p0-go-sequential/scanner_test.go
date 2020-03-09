92a00f473848142c8906f3435a17ec883dee5faf
Raymond Tu <tur1@mcmaster.ca>
2020-03-09 15:10:04 -0400

    Bugfixes

---
+++ p0-go-sequential/scanner_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestInitialization(t *testing.T) {
+	scannerInit("potato")
+	//if found.GetP0Type().p0primitive != Int {
+	//	t.Errorf("The type of variable potate was incorrect")
+	//}
+}
