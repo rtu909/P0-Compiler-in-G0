327c0821b8f0be830454461f52c32eddcef08125
ROHIT SAILY <sailyr1@mcmaster.ca>
2020-03-09 11:29:45 -0400

    Partially implement selector
    Functions were added to Entry to indicate what is
    needed of the selector function.

--- p0-go-sequential/parser.go
+++ p0-go-sequential/parser.go
@@ -12,3 +12,67 @@ var FIRSTDECL = [4]int{CONST, TYPE, VAR, PROCEDURE}
 var FOLLOWDECL = [1]int{BEGIN}
 var FOLLOWPROCCALL = [3]int{SEMICOLON, END, ELSE}
 var STRONGSYMS = [8]int{CONST, TYPE, VAR, PROCEDURE, WHILE, IF, BEGIN, EOF}
+
+func selector(x Entry) Entry {
+	var a = [2]int{PERIOD, LBRAK}
+	for doesContain(a[:], sym) {
+		if sym == PERIOD { // x.f
+			getSym()
+			if sym == IDENT {
+				if x.GetP0Type() == Record {
+					for f := range x.GetFieldNames() {
+						if f == val {
+							// x = CG.genSelect(x, f);
+							break
+						} else {
+							mark("not a field")
+						}
+					}
+					getSym()
+				} else {
+					mark("not a record")
+				}
+			} else {
+				mark("identifier expected")
+			}
+		} else { // x[y]
+			getSym()
+			var y = expression()
+			if x.GetP0Type() == Array {
+				if y.GetP0Type() == Int {
+					var value = y.GetValue()
+					var lowerbound = x.GetLowerBound()
+					if y.IsConstant() && value < lowerbound || y.GetValue() >= lowerbound+x.GetLength() {
+						mark("index out of bounds")
+					} else {
+						// x = CG.genIndex(x, y)
+					}
+				} else {
+					mark("index not integer")
+				}
+			} else {
+				mark("not an array")
+			}
+			if sym == RBRAK {
+				getSym()
+			} else {
+				mark("] expected")
+			}
+		}
+	}
+	return x
+}
+
+// TODO: IMPLEMENT
+func expression() Entry {
+	return nil
+}
+
+func doesContain(elements []int, e int) bool {
+	for _, a := range elements {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
