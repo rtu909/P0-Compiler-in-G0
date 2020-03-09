97a61d5456518c65587288edb0c0c97fa11869bb
David Thompson <thompd10@mcmaster.ca>
2020-03-08 23:15:22 -0400

    Some more thinking about the symboltable.go

--- p0-go-sequential/symboltable.go
+++ p0-go-sequential/symboltable.go
@@ -1,31 +1,41 @@
 package main
 
 // SymbolTable keeps track of declarations that are made in P0 source code.
+// It keeps track of the current scope, and queries can be made against it in order to find what declaration a symbol refers to.
 type SymbolTable interface {
 	Init()
 	NewDecl(string, Entry)
-	Find(string)
+	Find(string) Entry
 	OpenScope()
-	TopScope()
 	CloseScope()
+	TopScope() // TODO: how to implement, b/c the parser needs to work with this directly
 }
 
-// P0Type represents one of the built-in types in P0
-// Current implementation is an enumerated type; this will have to be changed, because Arrays also consist of a type
-// that is held, and records are sequences of types
-type P0Type int
+// P0Primitive is an enumerated type that represents one of the built-in types in P0.
+// It is only meant to represent the base types; composite types are represented in P0Type
+type P0Primitive int
 
 const (
-	Int P0Type = iota
+	Int P0Primitive = iota
 	Bool
-	Enum
 	Record
 	Array
 	None
 )
 
-// Entry represents items that can be put into the SymbolTable. All entries must have a DeclType (but it can be nil)
-// Some entries contain more data
+// P0Type is a representation of composite data types in P0.
+// It consists of the base type, p0primitive, combined with the constituent types, typeComponents.
+// If the base type is one of Int, Bool, or None, typeComponents can be nil and should not be accessed.
+// If the base type is Array, typeComponents must be of length 1 and contain the type that the array holds.
+// If the base type is Record, typeComponents must be of length 1 or greater.
+// The values represent the types of the fields, in the order that they appear in the Record.
+type P0Type struct {
+	p0primitive    P0Primitive
+	typeComponents []P0Type
+}
+
+// Entry represents items that can be put into the SymbolTable.
+// All entries have a type.
 type Entry interface {
 	GetP0Type() P0Type
 }
@@ -33,32 +43,33 @@ type Entry interface {
 // P0Var represents an entry in the symbol table for a P0 variable
 type P0Var P0Type
 
+// P0Var implements the Entry interface so that it can be stored in the symbol table.
 func (p0var P0Var) GetP0Type() P0Type {
 	return P0Type(p0var)
 }
 
-// P0Const represents an identifier that s linked to a constant value
+// P0Const represents an identifier that is linked to a constant value.
 type P0Const struct {
 	p0type P0Type
 	value  interface{} //TODO: what needs to go here?
 }
 
+// P0Const implements the Entry interface so that it can be stored in the symbol table
 func (p0const P0Const) GetP0Type() P0Type {
 	return p0const.p0type
 }
 
-// ArraySymbolTable implements the symbol table as an array of maps from strings to the Entry
-type ArraySymbolTable []map[string]Entry
+// SliceMapSymbolTable implements the symbol table as a slice of maps from string to Entry
+type SliceMapSymbolTable []map[string]Entry
 
-// Init initialized an ArraySymbolTable for use
-// It sets up the outermost context for use
-func (st *ArraySymbolTable) Init() {
+// Init initialized a SliceMapSymbolTable for use.
+func (st *SliceMapSymbolTable) Init() {
 	*st = make([]map[string]Entry, 1)
 	(*st)[0] = make(map[string]Entry)
 }
 
-// NewDecl adds a new declaration to the symbol table at the current level
-func (st *ArraySymbolTable) NewDecl(name string, entry Entry) {
+// NewDecl adds a new declaration to the symbol table at the current level.
+func (st *SliceMapSymbolTable) NewDecl(name string, entry Entry) {
 	_, present := (*st)[len(*st)-1][name]
 	if !present {
 		(*st)[len(*st)-1][name] = entry
@@ -68,9 +79,9 @@ func (st *ArraySymbolTable) NewDecl(name string, entry Entry) {
 }
 
 // Find attempts to find the innermost declaration of the symbol `name` in the symbol table.
-// If it is found, the entry is returned
-// If it is not found, a P0Const with value 0 and P0Type None is returned
-func (st *ArraySymbolTable) Find(name string) Entry {
+// If it is found, the corresponding entry is returned.
+// If it is not found, a P0Const with value 0 and P0Type None (eg. P0Type{None, nil}) is returned.
+func (st *SliceMapSymbolTable) Find(name string) Entry {
 	for i := len(*st) - 1; i >= 0; i-- {
 		entry, present := (*st)[i][name]
 		if present {
@@ -78,18 +89,17 @@ func (st *ArraySymbolTable) Find(name string) Entry {
 		}
 	}
 	println("Cannot find symbol")
-	return P0Const{None, 0}
+	return P0Const{P0Type{None, nil}, 0}
 }
 
-// OpenScope opens a new (innermost) declaration scoping.
-// This means that symbols defined in the current scope could be redefined
-func (st *ArraySymbolTable) OpenScope() {
+// OpenScope opens a new (innermost) declaration scope.
+func (st *SliceMapSymbolTable) OpenScope() {
 	*st = append(*st, make(map[string]Entry))
 }
 
 // CloseScope closes the innermost scope of the symbol table.
-// Any declarations made in this scope are deleted.
+// Any declarations made in the scope are deleted.
 // The new innermost scope becomes the old second most inner scope.
-func (st *ArraySymbolTable) CloseScope() {
+func (st *SliceMapSymbolTable) CloseScope() {
 	*st = (*st)[0 : len(*st)-1]
 }
--- p0-go-sequential/symboltable_test.go
+++ p0-go-sequential/symboltable_test.go
@@ -3,19 +3,19 @@ package main
 import "testing"
 
 func TestVariableFind(t *testing.T) {
-	var st ArraySymbolTable
-	var entry P0Var
+	st := new(SliceMapSymbolTable)
+	myVar := P0Var{Int, nil}
 	st.Init()
-	st.NewDecl("potate", entry)
-	found := st.Find("potate")
-	if found.GetP0Type() != Int {
+	st.NewDecl("potato", myVar)
+	found := st.Find("potato")
+	if found.GetP0Type().p0primitive != Int {
 		t.Errorf("The type of variable potate was incorrect")
 	}
 }
 
 func TestConstantFind(t *testing.T) {
-	var st ArraySymbolTable
-	myConst := P0Const{Int, 67}
+	st := new(SliceMapSymbolTable)
+	myConst := P0Const{P0Type{Int, nil}, 67}
 	st.Init()
 	st.NewDecl("i", myConst)
 	found := st.Find("i")
@@ -23,23 +23,71 @@ func TestConstantFind(t *testing.T) {
 	if !castOk {
 		t.Errorf("The declaration couldn't be retrieved as a const")
 	}
-	if castFound.p0type != Int || castFound.value != 67 {
+	if castFound.p0type.p0primitive != Int || castFound.value != 67 {
 		t.Errorf("The type or value of the const was incorrect")
 	}
 }
 
 func TestEmptyFind(t *testing.T) {
-	var st ArraySymbolTable
+	st := new(SliceMapSymbolTable)
 	st.Init()
 	found := st.Find("item")
-	if found.GetP0Type() != None {
-		t.Errorf("The type was supposed to be nil, but was %v", found.GetP0Type())
+	if found.GetP0Type().p0primitive != None {
+		t.Errorf("The type was supposed to be nil, but was %v", found.GetP0Type().p0primitive)
 	}
 	foundConst, castOk := found.(P0Const)
 	if !castOk {
 		t.Errorf("A P0Const was expected, but not returned")
 	}
-	if foundConst.p0type != None || foundConst.value != 0 {
-		t.Errorf("The value ")
+	if foundConst.p0type.p0primitive != None || foundConst.value != 0 {
+		t.Errorf("The value was %v, but was expected to be 0", found.GetP0Type().p0primitive)
+	}
+}
+
+func TestDeclDroppedOutOfScope(t *testing.T) {
+	st := new(SliceMapSymbolTable)
+	myConst := P0Var{Int, nil}
+	st.Init()
+	st.OpenScope()
+	st.NewDecl("potato", myConst)
+	st.CloseScope()
+	found := st.Find("potato")
+	castFound, castOk := found.(P0Const)
+	if !castOk || castFound.GetP0Type().p0primitive != None || castFound.value != 0 {
+		t.Errorf("A declaration of type %v, value %v was found, but there should be no variables in scope", castFound.GetP0Type().p0primitive, castFound.value)
+	}
+}
+
+func TestOuterDeclarationFound(t *testing.T) {
+	st := new(SliceMapSymbolTable)
+	myConst1 := P0Const{P0Type{Int, nil}, 42}
+	myConst2 := P0Const{P0Type{Int, nil}, 68}
+	st.Init()
+	st.NewDecl("potato", myConst1)
+	st.OpenScope()
+	st.NewDecl("potato", myConst2)
+	found := st.Find("potato").(P0Const)
+	if found.value != 68 {
+		t.Errorf("Picked up a value of %v, expect 68", found.value)
+	}
+	st.CloseScope()
+	found = st.Find("potato").(P0Const)
+	if found.value != 42 {
+		t.Errorf("Picked up a value of %v, expected 42", found.value)
+	}
+}
+
+func TestFindDeclarationFromInnerLabel(t *testing.T) {
+	st := new(SliceMapSymbolTable)
+	myConst := P0Const{P0Type{Int, nil}, 42}
+	st.Init()
+	st.NewDecl("potato", myConst)
+	st.OpenScope()
+	found, castOk := st.Find("potato").(P0Const)
+	if !castOk {
+		t.Error("Found something of an unexpected type")
+	}
+	if found.value != 42 {
+		t.Errorf("Found a value of %v, but expected 42", myConst)
 	}
 }
