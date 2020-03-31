95561dd672f79509406023493f78d28ce14d05db
David Thompson <thompd10@mcmaster.ca>
2020-03-31 10:52:50 -0400

    Starting work on concurrent

--- p0-go-concurrent/p0-go-concurrent.go
+++ p0-go-concurrent/p0-go-concurrent.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func main() {
-	compileFile("../p0-programs/Fibonacci.p", "mips")
+
+	// Open a file for buffered reading
+	f, err := os.Open("../p0-programs/Fibonacci.p")
+	if err {
+		panic("Unable to open the requested file")
+	}
+	reader := bufio.NewReader(f)
+	// Make a channel to connect the scanner and the parser
+	tokenChannel := make(chan int, 5)
+	endChannel := make(chan int)
+	// start the parser
+	ScannerInit(reader, tokenChannel)
+	// start the scanner
+	compileFile(tokenChannel, endChannel, "wat")
+	<-endChannel
 }
