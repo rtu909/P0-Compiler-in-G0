ac89dd90f1015d1b16078c3593a590236776a999
Rohit Saily <sailyr1@mcmaster.ca>
2020-04-01 10:56:54 -0400

    Make error messages nicer

--- p0-go-concurrent/p0-go-concurrent.go
+++ p0-go-concurrent/p0-go-concurrent.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"bufio"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
 
-	sourceFilePath := "../p0-programs/Fibonacci.p"
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		panic("ERROR: Expected one command line argument but got " + string(len(args)))
+		os.Exit(1)
+	}
+	sourceFilePath := args[0]
+	if !isAccessibleFile(sourceFilePath) {
+		panic("ERROR: Accessing: " + sourceFilePath + " failed. Ensure that it exists and read & write permissions are enabled.")
+		os.Exit(1)
+	}
+
 	var destFilePath string
 
 	// Open a file for buffered reading
@@ -31,4 +43,18 @@ func main() {
 	// start the scanner
 	go compileFile(tokenChannel, endChannel, destFilePath, "wat")
 	<-endChannel
+}
+
+func isAccessibleFile(filePath string) bool {
+	_, err := os.Stat(filePath)
+	if err == nil {
+		return true // Path to an existing file
+	}
+	var contents []byte
+	err = ioutil.WriteFile(filePath, contents, 0644) // 0644 = -rw-r--r--
+	if err == nil {
+		os.Remove(filePath)
+		return false // Path to an existing accessible directory
+	}
+	return false // Failed to access
 }
